5f131b6f92517bdb178f42c4af7f628a8eefcff9
alex <alexs123655@gmail.com>
2022-06-24 21:44:43 -0300

    feat: add process env port

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,6 +1,6 @@
 import { app } from "./app";
 
-const port = 5000 || process.env.PORT;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
